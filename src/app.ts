08c52307806d1a91653aef8f8d8b76b53f009840
Dmytro Semenov <samtvone@gmail.com>
2019-09-02 17:31:47 +0300

    types testing

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
-console.log('Hello TypeScript!');
+type Pizza = { name: string; toppings: number };
+
+const pizza: Pizza = { name: 'Blazing Inferno', toppings: 5 };
+
+const serialized = JSON.stringify(pizza);
+console.log(serialized);
